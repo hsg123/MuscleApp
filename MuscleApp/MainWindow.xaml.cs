075eda9faf0371331380d5af4ef5f7067ed22af5
Harry <harinder950@gmail.com>
2016-07-28 20:52:16 +1000

    Second Commit

--- MuscleApp/MainWindow.xaml.cs
+++ MuscleApp/MainWindow.xaml.cs
@@ -22,9 +22,10 @@ namespace MuscleApp
     public partial class MainWindow : Window
     {
 
-        private Polygon p = new Polygon();
+        //private Polygon p = new Polygon();
         private double imgWidth, imgHeight;
-        private Image img; 
+        private Image img;
+        private Body body;
 
         public MainWindow()
         {
@@ -36,25 +37,14 @@ namespace MuscleApp
                 imgWidth = img.ActualWidth;
                 imgHeight = img.ActualHeight;
                 // access ActualWidth and ActualHeight here
-                initPolygons();
+                body = FileHandler.GetPolygonsFromFile("Body.xml");
+                initBodyPolygons();
+                //initPolygons();
                 canvas.Width = img.ActualWidth;
                 canvas.Height = img.ActualHeight;
-               // TextBox tb = new TextBox();
-                canvas.Children.Add(p);
-                desc.Visibility = Visibility.Hidden;
-                //canvas.Children.Add(tb);
-                //tb.Text = "Hello";
-
-                //tb.Height = imgHeight * (0.13919 - 0.043956);
-                //tb.Width = imgWidth * (0.665 - 0.3112);
-                /*tb.RenderTransform = new TranslateTransform
-                {
-                    X = imgWidth * 0.3112,
-                    Y = imgHeight * 0.043956
-                };*/
-               
+                //canvas.Children.Add(p);
+                desc.Visibility = Visibility.Hidden;         
                 desc.Text = "Hello";
-
                 desc.Height = imgHeight * (0.13919 - 0.043956);
                 desc.Width = imgWidth * (0.665 - 0.3112);
                 desc.RenderTransform = new TranslateTransform
@@ -62,11 +52,13 @@ namespace MuscleApp
                     X = imgWidth * 0.3112,
                     Y = imgHeight * 0.043956
                 };
+                canvas.Children.Add(body.Muscles[0].MuscleShape);
             };
+        }
 
-         
-           
-
+        private void initBodyPolygons()
+        {
+            body.Scale(img.ActualWidth, img.ActualHeight);
         }
 
         private void Image_DragOver(object sender, DragEventArgs e)
@@ -75,74 +67,36 @@ namespace MuscleApp
         }
 
        
-        public void initPolygons()
+        /*public void initPolygons()
         {
             // Create a blue and a black Brush
             SolidColorBrush yellowBrush = new SolidColorBrush();
             yellowBrush.Color = Colors.Yellow;
             SolidColorBrush blackBrush = new SolidColorBrush();
             blackBrush.Color = Colors.Black;
-
-           
-           
             p.Stroke = blackBrush;
             p.Fill = yellowBrush;
-            p.StrokeThickness = 4;
+            p.StrokeThickness = 0;
             p.Points.Add(new Point(0,0));//top left corner
             p.Points.Add(new Point(imgWidth/10, 0));//top right corner
             p.Points.Add(new Point(imgWidth / 10, img.ActualHeight/10));//bottom right corner
             p.Points.Add(new Point(0, imgHeight/10));//bottom left corner
+            p.Opacity = 0;
         }
-        
+        */
         
         private void canvas_MouseMove(object sender, MouseEventArgs e)
         {
-            /* Point mousePt = e.GetPosition((Canvas)sender);//mouse point relative to image
-             if (pnpoly(p.Points.Count, mousePt.X, mousePt.Y))
-             {
-                 p.Opacity = 0;
-                 desc.Visibility = Visibility.Visible; 
-             }else
-             {
-                 //p.Visibility = Visibility.Hidden;
-                 desc.Visibility = Visibility.Hidden;
-             }*/
-            p.Opacity = 100;
-            //desc.Opacity = 100;
-
+            //p.Opacity = 100;
             desc.Visibility = Visibility.Visible;
         }
 
         private void canvas_MouseLeave(object sender, MouseEventArgs e)
-        {
-            /*Point mousePt = e.GetPosition((Canvas)sender);//mouse point relative to image
-            if (pnpoly(p.Points.Count, mousePt.X, mousePt.Y))
-            {
-                p.Opacity = 0;
-                desc.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                //p.Visibility = Visibility.Hidden;
-                desc.Visibility = Visibility.Hidden;
-            }*/
-            p.Opacity = 0;
-            //desc.Opacity = 0;
-
+        {      
+            //p.Opacity = 0;
             desc.Visibility = Visibility.Hidden;
         }
 
-        bool pnpoly(int nvert, double testx, double testy)
-        {
-            int i, j;
-            bool c = false;
-            for (i = 0, j = nvert - 1; i < nvert; j = i++)
-            {
-                if (((p.Points.ElementAt(i).Y > testy) != (p.Points.ElementAt(j).Y > testy)) &&
-                 (testx < (p.Points.ElementAt(j).X - p.Points.ElementAt(i).X) * (testy - p.Points.ElementAt(i).Y) / (p.Points.ElementAt(j).Y - p.Points.ElementAt(i).Y) + p.Points.ElementAt(i).X))
-                    c = !c;
-            }
-            return c;
-        }
+        
     }
 }
